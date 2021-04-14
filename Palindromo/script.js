8cf42333cc9b1c994d3f0e4500f729e6b546378b
PietroCampodonico <pietro.campodonico@outlook.it>
2021-04-14 14:38:06 +0200

    Inizializzazione progetto

---
+++ Palindromo/script.js
@@ -0,0 +1,10 @@
+/*Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma*/
+
+
+var inputUtente = prompt("Inserisci una parola e ti saprò dire se è un palindromo!");
+
+for (i = 0; i < inputUtente.length; i++)
+    
+
+function isthispalindrome();
